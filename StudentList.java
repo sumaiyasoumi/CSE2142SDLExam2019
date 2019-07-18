af27495a554f5a8b6a5f68b2875d9feed388eaa1
jubaer <mdjasont@gmail.com>
2019-07-18 14:53:57 +0600

    Update code style for better consistency

--- StudentList.java
+++ StudentList.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class StudentList {
     public static void main(String[] args) {
 
+        }
+
 //		Check arguments
         if (args[0].equals("a")) {
             System.out.println("Loading data ...");
